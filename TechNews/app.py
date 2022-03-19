b2cd9c54a3ed6f6ce3eee6b0fe5d1e326a8e3bb5
remote007 <pmnrai2013@gmail.com>
2022-03-19 11:41:08 +0530

    Update app.py

--- TechNews/app.py
+++ TechNews/app.py
@@ -6,4 +6,4 @@ url ="https://www.businesstoday.in/technology/news"
 req = requests.get(url)
 # soup.a for anchor tag and soup.find_all('a') to give a list of all anchor tags
 soup = BeautifulSoup(req.content,"html.parser")
-outerdata = soup.find_all("div",class_="widget-listing",limit=6) #
+outerdata = soup.find_all("div",class_="widget-listing",limit=6) 
