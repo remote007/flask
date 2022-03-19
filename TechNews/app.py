042ef8ec8e8b16b560ed8d8407ddae9220fbc8e3
remote007 <pmnrai2013@gmail.com>
2022-03-19 11:33:30 +0530

    update

--- TechNews/app.py
+++ TechNews/app.py
@@ -1,8 +1,9 @@
-from flask import flask,render_template
+# from Flask import flask,render_template
 from bs4 import BeautifulSoup
 import requests
 
 url ="https://www.businesstoday.in/technology/news"
 req = requests.get(url)
+# soup.a for anchor tag and soup.find_all('a') to give a list of all anchor tags
 soup = BeautifulSoup(req.content,"html.parser")
 print(soup)
