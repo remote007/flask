a40069eac18ee09566387eaba4a08a4b45f25cf0
remote007 <pmnrai2013@gmail.com>
2022-03-19 19:09:23 +0530

    Update wiki_scraper.py

--- wikiscraper_project/wiki_scraper.py
+++ wikiscraper_project/wiki_scraper.py
@@ -1,7 +1,12 @@
+from urllib import response
 import requests
 import json
 from bs4 import BeautifulSoup
 
 def create_api_data():
     url = "https://type.fit/api/quotes"
-    
+    response = requests.get(url)
+    return response.json()
+
+def create_quotes_dict(api_data):
+    quotes = {}
