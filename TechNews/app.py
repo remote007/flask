b5b7805b1ff895164d8ac48805f9d768c6a7b85d
remote007 <pmnrai2013@gmail.com>
2022-03-19 14:45:58 +0530

    Update app.py

--- TechNews/app.py
+++ TechNews/app.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__) 
-@app.route("/",methods=["Get","POST"])
-url ="https://www.businesstoday.in/technology/news"
-req = requests.get(url)
-# soup.a for anchor tag and soup.find_all('a') to give a list of all anchor tags
-soup = BeautifulSoup(req.content,"html.parser")
-outerdata = soup.find_all("div",class_="widget-listing",limit=6) 
-finalnews =""
-for news in outerdata:
-    finalnews +="\u2022 "+news.div.div.a["title"]+"\n"
-print(finalnews)    
+@app.route("/",methods=["Get","POST"]) #@ means decorate , it is always above a function
+def index() :
+
+    url ="https://www.businesstoday.in/technology/news"
+    req = requests.get(url)
+    # soup.a for anchor tag and soup.find_all('a') to give a list of all anchor tags
+    soup = BeautifulSoup(req.content,"html.parser")
+    outerdata = soup.find_all("div",class_="widget-listing",limit=6) 
+    finalnews =""
+    for news in outerdata:
+        finalnews +="\u2022 "+news.div.div.a["title"]+"\n"
+    print(finalnews)    
